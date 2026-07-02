64840cab0fcb9fa082a02a1b9c9b85a8b3dce0dc
Josh Montoya <josh@montoya.io>
2026-07-02 15:10:43 -0700

    implement L2WeightDecay

---
+++ optimizer/l2_weight_decay.go
@@ -0,0 +1,67 @@
+package optimizer
+
+// NewL2WeightDecay constructs L2 weight decay with the provided coefficient.
+func NewL2WeightDecay(coefficient float64) (out *L2WeightDecay, err error) {
+	if err = validateRegularizationCoefficient("l2 weight decay", coefficient); err != nil {
+		return nil, err
+	}
+
+	var l L2WeightDecay
+	l.coefficient = coefficient
+	return &l, nil
+}
+
+// L2WeightDecay adds coefficient*value to each parameter gradient.
+type L2WeightDecay struct {
+	coefficient float64
+}
+
+// Apply adds L2 weight decay gradients to parameters.
+func (l *L2WeightDecay) Apply(parameters []*Parameter) (err error) {
+	if err = l.validate(); err != nil {
+		return err
+	}
+
+	err = applyRegularizationGradient(parameters, l.gradient)
+	return err
+}
+
+// Coefficient returns the L2 weight decay coefficient.
+func (l *L2WeightDecay) Coefficient() (coefficient float64) {
+	if l == nil {
+		return 0
+	}
+
+	coefficient = l.coefficient
+	return coefficient
+}
+
+// SetCoefficient updates the L2 weight decay coefficient.
+func (l *L2WeightDecay) SetCoefficient(coefficient float64) (err error) {
+	if l == nil {
+		err = nilRegularizerError("l2 weight decay")
+		return err
+	}
+
+	if err = validateRegularizationCoefficient("l2 weight decay", coefficient); err != nil {
+		return err
+	}
+
+	l.coefficient = coefficient
+	return nil
+}
+
+func (l *L2WeightDecay) gradient(value float64) (gradient float64) {
+	gradient = l.coefficient * value
+	return gradient
+}
+
+func (l *L2WeightDecay) validate() (err error) {
+	if l == nil {
+		err = nilRegularizerError("l2 weight decay")
+		return err
+	}
+
+	err = validateRegularizationCoefficient("l2 weight decay", l.coefficient)
+	return err
+}
