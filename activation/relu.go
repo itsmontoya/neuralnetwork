16d4a47889a4653b1b244dcef1c4b6f11b166bee
Josh Montoya <josh@montoya.io>
2026-07-02 12:06:49 -0700

    implement ReLU

---
+++ activation/relu.go
@@ -0,0 +1,35 @@
+package activation
+
+import "github.com/itsmontoya/neuralnetwork/matrix"
+
+// ReLU applies the rectified linear unit activation.
+type ReLU struct{}
+
+// Forward returns max(0, x) for each input value.
+func (r ReLU) Forward(input *matrix.Matrix) (output *matrix.Matrix, err error) {
+	output, err = apply(input, reLUValue)
+	return output, err
+}
+
+// Backward multiplies outputGradient by the ReLU derivative at input.
+func (r ReLU) Backward(input, outputGradient *matrix.Matrix) (inputGradient *matrix.Matrix, err error) {
+	inputGradient, err = applyDerivative(input, outputGradient, reLUDerivative)
+	return inputGradient, err
+}
+
+func reLUValue(value float64) (result float64) {
+	if value > 0 {
+		result = value
+		return result
+	}
+
+	return 0
+}
+
+func reLUDerivative(value float64) (result float64) {
+	if value > 0 {
+		return 1
+	}
+
+	return 0
+}
