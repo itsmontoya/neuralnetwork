22937754ff5d30be364f54973220220bf1acbadb
Josh Montoya <josh@montoya.io>
2026-07-03 09:39:45 -0700

    implement Batch

---
+++ data/batch.go
@@ -0,0 +1,73 @@
+package data
+
+import "github.com/itsmontoya/neuralnetwork/matrix"
+
+func newBatch(inputs, targets *matrix.Matrix) (out *Batch, err error) {
+	var (
+		clonedInputs  *matrix.Matrix
+		clonedTargets *matrix.Matrix
+		b             Batch
+	)
+
+	if err = validateMatrixPair("batch inputs", inputs, "batch targets", targets); err != nil {
+		return nil, err
+	}
+
+	if clonedInputs, err = inputs.Clone(); err != nil {
+		return nil, err
+	}
+
+	if clonedTargets, err = targets.Clone(); err != nil {
+		return nil, err
+	}
+
+	b.inputs = clonedInputs
+	b.targets = clonedTargets
+	return &b, nil
+}
+
+// Batch contains paired input and target matrices for one mini-batch.
+type Batch struct {
+	inputs  *matrix.Matrix
+	targets *matrix.Matrix
+}
+
+// Inputs returns a copy of the batch inputs.
+func (b *Batch) Inputs() (inputs *matrix.Matrix, err error) {
+	if err = b.validate(); err != nil {
+		return nil, err
+	}
+
+	inputs, err = b.inputs.Clone()
+	return inputs, err
+}
+
+// Targets returns a copy of the batch targets.
+func (b *Batch) Targets() (targets *matrix.Matrix, err error) {
+	if err = b.validate(); err != nil {
+		return nil, err
+	}
+
+	targets, err = b.targets.Clone()
+	return targets, err
+}
+
+// SampleCount returns the number of paired samples in the batch.
+func (b *Batch) SampleCount() (samples int) {
+	if b == nil || b.inputs == nil {
+		return 0
+	}
+
+	samples = b.inputs.Rows()
+	return samples
+}
+
+func (b *Batch) validate() (err error) {
+	if b == nil {
+		err = nilBatchError()
+		return err
+	}
+
+	err = validateMatrixPair("batch inputs", b.inputs, "batch targets", b.targets)
+	return err
+}
