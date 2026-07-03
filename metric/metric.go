c8fb30157fbb4467ec350a6b5b6bc2603830d2cb
Josh Montoya <josh@montoya.io>
2026-07-03 11:16:53 -0700

    implement Metric

---
+++ metric/metric.go
@@ -0,0 +1,10 @@
+// Package metric defines reporting metrics separate from optimization losses.
+package metric
+
+import "github.com/itsmontoya/neuralnetwork/matrix"
+
+// Metric evaluates predictions against targets for reporting.
+type Metric interface {
+	// Value computes a reporting metric from predictions and targets.
+	Value(predictions, targets *matrix.Matrix) (value float64, err error)
+}
---
+++ metric/metric_test.go
@@ -0,0 +1,28 @@
+package metric_test
+
+import (
+	"testing"
+
+	"github.com/itsmontoya/neuralnetwork/matrix"
+	"github.com/itsmontoya/neuralnetwork/metric"
+)
+
+func Test_Metric_Interface(t *testing.T) {
+	var _ metric.Metric = metric.MeanSquaredError{}
+	var _ metric.Metric = metric.BinaryAccuracy{}
+	var _ metric.Metric = metric.BinaryPrecision{}
+	var _ metric.Metric = metric.BinaryRecall{}
+	var _ metric.Metric = metric.BinaryF1{}
+	var _ metric.Metric = metric.CategoricalAccuracy{}
+	var _ metric.Metric = metric.CategoricalMacroPrecision{}
+	var _ metric.Metric = metric.CategoricalMacroRecall{}
+	var _ metric.Metric = metric.CategoricalMacroF1{}
+	var _ metric.Metric = mockMetric{}
+}
+
+type mockMetric struct{}
+
+func (m mockMetric) Value(predictions, targets *matrix.Matrix) (value float64, err error) {
+	value = 0
+	return value, nil
+}
